482d1091e543a9bc3a4018ba8ecbd3d645a70c20
Boris Pinus <piboro@yandex-team.ru>
2024-08-04 19:43:24 +0300

    test

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,20 +1,25 @@
 import ips from 'node:os';
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
+import { headers } from 'next/headers';
 
-const handler = async (request: NextApiRequest, response: NextApiResponse<any>) => {
-    const ip = request.socket.remoteAddress;
-    const res = await fetch(`http://ip-api.com/json/${ip}`, {
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-    const data = await res.json();
+const handler = async (
+  request: NextApiRequest,
+  response: NextApiResponse<any>
+) => {
+  const ip = request.socket.remoteAddress;
+  const res = await fetch(`http://ip-api.com/json/${ip}`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await res.json();
 
-    response.status(200).json({
-        local: request.socket.localAddress,
-        remote: request.socket.remoteAddress,
-        data: ips.networkInterfaces()
-    })
+  response.status(200).json({
+    local: request.socket.localAddress,
+    remote: request.socket.remoteAddress,
+    data: ips.networkInterfaces(),
+    headers: request.headers,
+  });
 };
 
 export default handler;
