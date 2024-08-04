8a099c21d6ef3d25f65b8f858d4ebb30624220c5
Boris Pinus <piboro@yandex-team.ru>
2024-08-04 19:27:27 +0300

    test

---
+++ src/pages/api/test.ts
@@ -0,0 +1,19 @@
+import ips from 'node:os';
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (request: NextApiRequest, response: NextApiResponse<any>) => {
+    const ip = request.socket.remoteAddress;
+    const res = await fetch(`http://ip-api.com/json/${ip}`, {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    const data = await res.json();
+
+    response.status(200).json({
+        local: request.socket.localAddress,
+        remote: request.socket.remoteAddress
+    })
+};
+
+export default handler;
